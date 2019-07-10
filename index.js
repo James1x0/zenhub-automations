9c63f7bcf1354be34892ba327c57076524c50d38
James <james@aehr.org>
2019-07-10 14:02:23 -0600

    Adds pull_request opened test

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const { Toolkit } = require('actions-toolkit')
+const { Toolkit } = require('actions-toolkit'),
+      { join } = require('path');
 
 const tools = new Toolkit();
 
+Toolkit.run(async tools => {
+  const handlerRef = `${tools.context.event}.js`;
 
-console.log(tools.context);
+  tools.log.info(`Trying to load handler: "${handlerRef}"...`);
+
+  try {
+    var eventModule = require(join('./lib/handlers', handlerRef));
+  } catch (e) {
+    return tools.exit.neutral('Failed to load module for event. No action necessary.');
+  }
+
+  if (!eventModule[tools.context.payload.action]) {
+    return tools.exit.neutral('Failed to find sub handler. No action necessary.');
+  }
+
+  try {
+    await eventModule(tools);
+  } catch (e) {
+    return tools.exit.failure(`Failed to run event handler: ${e}`);
+  }
+
+  tools.exit.success('Executed event handler.');
+});
